de1d711935433bf4f7e54f660865bb3ed77da423
xsrf <git@xsrf.de>
2020-05-11 21:53:59 +0200

    add ICACHE_RAM_ATTR for Timer ISR in example

--- examples/timing.cpp
+++ examples/timing.cpp
@@ -6,19 +6,21 @@
 uint8_t* buffer;
 const uint8_t size = 140;
 
+ICACHE_RAM_ATTR void isr() {
+    NBSPI_TDBG_HIGH;
+    SPI.writeBytes(buffer,80);
+    NBSPI_TDBG_LOW;
+    delayMicroseconds(5);
+    nbSPI_writeBytes(buffer,80);
+}
+
 void setup() {
   buffer = (uint8_t*) calloc(size, sizeof(byte));
   for(uint16_t i=0; i<size; i++) buffer[i] = i;
   SPI.begin();
   SPI.setFrequency(8e6);
   pinMode(NBSPI_TDBG_IO, OUTPUT);
-  timer1_attachInterrupt([](){ 
-    NBSPI_TDBG_HIGH;
-    SPI.writeBytes(buffer,80);
-    NBSPI_TDBG_LOW;
-    delayMicroseconds(5);
-    nbSPI_writeBytes(buffer,80);
-  });
+  timer1_attachInterrupt(isr);
   timer1_enable(TIM_DIV16, TIM_EDGE, TIM_LOOP);
   timer1_write(5*300); // 5 ticks per µs
 }
